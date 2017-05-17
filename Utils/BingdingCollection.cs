5c8984375ec06c9fcaac4daeb86b1410a116eb0b
Astro <306805618@qq.com>
2017-05-17 16:42:39 +0800

    工具

---
+++ Utils/BingdingCollection.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ControlAstro.Utils
+{
+    public class BindingCollection<T> : BindingList<T>
+    {
+        private bool isSorted;
+        private PropertyDescriptor sortProperty;
+        private ListSortDirection sortDirection;
+
+        public BindingCollection() : base()
+        {
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="list">IList类型的列表对象</param>
+        public BindingCollection(IList<T> list) : base(list)
+        {
+        }
+
+        /// <summary>
+        /// 获取一个值，指示列表是否已排序
+        /// </summary>
+        protected override bool IsSortedCore
+        {
+            get { return isSorted; }
+        }
+
+        /// <summary>
+        /// 获取一个值，指示列表是否支持排序
+        /// </summary>
+        protected override bool SupportsSortingCore
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// 获取一个只，指定类别排序方向
+        /// </summary>
+        protected override ListSortDirection SortDirectionCore
+        {
+            get { return sortDirection; }
+        }
+
+        /// <summary>
+        /// 获取排序属性说明符
+        /// </summary>
+        protected override PropertyDescriptor SortPropertyCore
+        {
+            get { return sortProperty; }
+        }
+
+        /// <summary>
+        /// 获取一个值，指示列表是否支持搜索
+        /// </summary>
+        protected override bool SupportsSearchingCore
+        {
+            get { return true; }
+        }
+
+
+        protected override void ApplySortCore(PropertyDescriptor property, ListSortDirection direction)
+        {
+            List<T> items = this.Items as List<T>;
+
+            if (items != null)
+            {
+                ObjectPropertyCompare<T> pc = new ObjectPropertyCompare<T>(property, direction);
+                items.Sort(pc);
+                isSorted = true;
+            }
+            else
+            {
+                isSorted = false;
+            }
+
+            sortProperty = property;
+            sortDirection = direction;
+
+            this.OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+        }
+
+        /// <summary>
+        /// 移除默认实现的排序
+        /// </summary>
+        protected override void RemoveSortCore()
+        {
+            isSorted = false;
+            this.OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+        }
+
+        public void Sort(PropertyDescriptor property, ListSortDirection direction)
+        {
+            this.ApplySortCore(property, direction);
+        }
+    }
+
+    internal class ObjectPropertyCompare<T> : IComparer<T>
+    {
+        private PropertyDescriptor property;
+        private ListSortDirection direction;
+
+        public ObjectPropertyCompare(PropertyDescriptor property, ListSortDirection direction)
+        {
+            this.property = property;
+            this.direction = direction;
+        }
+
+        #region IComparer<T>
+
+        /// <summary>
+        /// 比较方法
+        /// </summary>
+        /// <param name="x">相对属性x</param>
+        /// <param name="y">相对属性y</param>
+        /// <returns></returns>
+        public int Compare(T x, T y)
+        {
+            object xValue = x.GetType().GetProperty(property.Name).GetValue(x, null);
+            object yValue = y.GetType().GetProperty(property.Name).GetValue(y, null);
+
+            int returnValue;
+
+            if (xValue is IComparable)
+            {
+                returnValue = ((IComparable)xValue).CompareTo(yValue);
+            }
+            else if (xValue.Equals(yValue))
+            {
+                returnValue = 0;
+            }
+            else
+            {
+                returnValue = xValue.ToString().CompareTo(yValue.ToString());
+            }
+
+            if (direction == ListSortDirection.Ascending)
+            {
+                return returnValue;
+            }
+            else
+            {
+                return returnValue * -1;
+            }
+        }
+
+        public bool Equals(T xWord, T yWord)
+        {
+            return xWord.Equals(yWord);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        #endregion
+    }
+}
---
+++ Utils/EncryptionTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ControlAstro.Utils
+{
+    public class EncryptionTools
+    {
+        public static string MD5(string inText)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] outText = md5.ComputeHash(Encoding.Default.GetBytes(inText));
+            return BitConverter.ToString(outText).Replace("-", "");
+        }
+    }
+}
