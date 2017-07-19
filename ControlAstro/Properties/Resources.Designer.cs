ab2ebec8702daa0c0e648f02ee0e5522d517b4c0
Astro <306805618@qq.com>
2017-07-19 17:32:09 +0800

    Resources

--- ControlAstro/Properties/Resources.Designer.cs
+++ ControlAstro/Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace ControlAstro.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap ImgCopyMode {
+            get {
+                object obj = ResourceManager.GetObject("ImgCopyMode", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap ImgMoveMode {
+            get {
+                object obj = ResourceManager.GetObject("ImgMoveMode", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
