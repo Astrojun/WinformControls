c36818ecb92bf3a039d0f6508de3293976f9c935
Astro <306805618@qq.com>
2017-07-19 17:32:26 +0800

    TipPanel

---
+++ ControlAstro/Controls/TipPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ControlAstro.Controls
+{
+    public partial class TipPanel : UserControl
+    {
+        public Image ImgInfo 
+        {
+            set
+            {
+                imgInfo.Image = value;
+            }
+            get
+            {
+                return imgInfo.Image;
+            }
+        }
+
+        public string InfoText
+        {
+            set
+            {
+                lblInfo.Text = value;
+            }
+            get
+            {
+                return lblInfo.Text;
+            }
+        }
+
+        public TipPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControlAstro/Controls/TipPanel.designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ControlAstro.Controls
+{
+    partial class TipPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.imgInfo = new System.Windows.Forms.PictureBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgInfo)).BeginInit();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.Controls.Add(this.imgInfo, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.panel, 1, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(148, 21);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // imgInfo
+            // 
+            this.imgInfo.Location = new System.Drawing.Point(3, 3);
+            this.imgInfo.Name = "imgInfo";
+            this.imgInfo.Size = new System.Drawing.Size(16, 15);
+            this.imgInfo.TabIndex = 1;
+            this.imgInfo.TabStop = false;
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.lblInfo);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(25, 3);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(200, 15);
+            this.panel.TabIndex = 2;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(3, 2);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(47, 12);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "lblInfo";
+            // 
+            // TipPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "TipPanel";
+            this.Size = new System.Drawing.Size(148, 21);
+            this.tableLayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgInfo)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.PictureBox imgInfo;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label lblInfo;
+
+    }
+}
