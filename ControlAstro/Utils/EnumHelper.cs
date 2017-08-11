2bbe62cc78f9b24a0f75f1c076fac6d0d29604eb
Astro <306805618@qq.com>
2017-08-11 17:31:41 +0800

    EnumHelper
    一些Enum的辅助方法

---
+++ ControlAstro/Utils/EnumHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ControlAstro.Utils
+{
+    public class EnumHelper
+    {
+        /// <summary>
+        /// 获取某个Enum值对应的DescriptionAttribute字符串，不存在则返回string.Empty
+        /// </summary>
+        /// <param name="en"></param>
+        /// <returns></returns>
+        public static string GetEnumDescription(Enum en)
+        {
+            var fieldInfo = en.GetType().GetField(en.ToString());
+            object[] objs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (objs.Length < 1)
+            {
+                return string.Empty;
+            }
+            foreach (var obj in objs)
+            {
+                if (obj is DescriptionAttribute)
+                {
+                    DescriptionAttribute attr = obj as DescriptionAttribute;
+                    return attr.Description;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
