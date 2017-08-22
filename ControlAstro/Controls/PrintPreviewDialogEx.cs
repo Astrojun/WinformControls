12956e3d82898a4a7b47f4d7ff8cde5ce4e75575
Astro <306805618@qq.com>
2017-08-22 09:37:51 +0800

    PrintPreviewDialogEx
    打印预览界面增加打印设置

---
+++ ControlAstro/Controls/PrintPreviewDialogEx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ControlAstro.Controls
+{
+    [ToolboxBitmap(typeof(PrintPreviewDialog))]
+    public partial class PrintPreviewDialogEx : PrintPreviewDialog
+    {
+        public PrintPreviewDialogEx()
+        {
+            InitializeComponent();
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is ToolStrip)
+                {
+                    ToolStrip tools = ctrl as ToolStrip;
+                    tools.Items.Insert(0, CreatePrintsetButton());
+                }
+            }
+        }
+
+        private ToolStripButton CreatePrintsetButton()
+        {
+            ToolStripButton Stripbutton = new ToolStripButton();
+            Stripbutton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            Stripbutton.Image = Properties.Resources.SettingTransparent;
+            Stripbutton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            Stripbutton.Name = "printsetStripButton";
+            Stripbutton.Size = new System.Drawing.Size(23, 22);
+            Stripbutton.Text = "打印设置";
+            Stripbutton.Click += Stripbutton_Click; ;
+            return Stripbutton;
+        }
+
+        private void Stripbutton_Click(object sender, EventArgs e)
+        {
+            using (PrintDialog diag = new PrintDialog())
+            {
+                diag.Document = Document;
+                diag.ShowDialog();
+            }
+        }
+    }
+}
