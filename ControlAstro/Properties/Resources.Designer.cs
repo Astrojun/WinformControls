86640cfc27b2629bb09967c490e77820d40be0a5
Astro <306805618@qq.com>
2017-08-22 09:38:16 +0800

    Controls
    更新打印设置图标

--- ControlAstro/Properties/Resources.Designer.cs
+++ ControlAstro/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace ControlAstro.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap SettingTransparent {
+            get {
+                object obj = ResourceManager.GetObject("SettingTransparent", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
